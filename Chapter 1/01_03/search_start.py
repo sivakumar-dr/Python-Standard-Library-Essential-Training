7e633fc59fe5f60a9636922119193cd747bc7109
sivakumar-dr <sivakumar.drr@gmail.com>
2024-02-24 20:48:39 +0530

    Ch-01-03 Basic String Search

--- Chapter 1/01_03/search_start.py
+++ Chapter 1/01_03/search_start.py
@@ -3,13 +3,21 @@
 
 sampleStr = "The quick brown fox jumps over the lazy dog"
 
-# TODO: startsWith and endsWith functions
+# convert to lower case
+# sampleStr = sampleStr.lower()
 
+# TODO: startsWith and endsWith functions
+# print(sampleStr.startswith("The"))
+# print(sampleStr.lower().startswith("the"))
+# print(sampleStr.endswith("dog"))
 
 # TODO: the find and rfind functions
-
+# print(sampleStr.find("the"))
+# print(sampleStr.rfind("the"))
+# print("the" in sampleStr)
 
 # TODO: using replace
-
+print(sampleStr.replace("lazy", "tired"))
 
 # TODO: counting instances of substrings
+print(sampleStr.count("over"))
